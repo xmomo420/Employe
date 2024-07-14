203a2186d75924d8609f986a13e43291122bd3f7
Mohamed Bendourou <hg991909@ens.uqam.ca>
2024-07-13 23:46:07 -0400

    Commit initial

---
+++ src/main/java/com/employe/EmployeApplication.java
@@ -0,0 +1,13 @@
+package com.employe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeApplication.class, args);
+    }
+
+}
